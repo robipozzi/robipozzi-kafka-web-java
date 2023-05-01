42ae6fadd1749f06e85cf2a9ceec2b5bcca4f75a
Roberto Pozzi <r.robipozzi@gmail.com>
2023-05-01 16:07:48 +0200

    update tests

--- src/test/java/com/rpozzi/kafka/KafkaWebApplicationTests.java
+++ src/test/java/com/rpozzi/kafka/KafkaWebApplicationTests.java
@@ -1,13 +1,8 @@
 package com.rpozzi.kafka;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class KafkaWebApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
